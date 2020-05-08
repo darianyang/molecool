94890b0b4827e8f7aa2d031e8bc8da5e90527588
Darian Yang <dty7@pitt.edu>
2020-05-08 17:05:53 -0400

    small adjustments to init and zen funct

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -3,8 +3,10 @@ molecool
 A python package for analyzing and visualizing pdb and xyz files. For MolSSI May webinar series.
 """
 
-# Add imports here
+# Add imports here (.) is look in same directory
+# import everything = *
 from .functions import *
+
 
 # Handle versioneer
 from ._version import get_versions
