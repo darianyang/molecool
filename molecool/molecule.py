ecf03b3aca3790e9bf55024eaa8b13ac65a01325
Darian Yang <dty7@pitt.edu>
2020-05-22 13:37:54 -0400

    added docstring to build_bond_list function.

--- molecool/molecule.py
+++ molecool/molecule.py
@@ -5,12 +5,31 @@ Molecule manipulations.
 from .measure import calculate_distance
 
 def build_bond_list(coordinates, max_bond=1.5, min_bond=0):
-    """
-    Find the bonds in a molecule (set of coordinates) based on distance criteria.
+    """Find the bonds in a molecule (set of coordinates) based on distance criteria.
+
+    The pairwise distance between atoms is computed. If it is in the range 
+    `min_bond` to `max_bond`, the atoms are counted as bonded.
+
+    Parameters
+    ----------
+    coordinates : ndarray
+        The coordinates of the atoms.
+    max_bond : float (optional)
+        The maximum distance for two atoms to be considered bonded. Default is 1.5.
+    min_bond : float (optional)
+        The minimum distance for two atoms to be considered bonded. Default is 0.
+
+    Returns
+    -------
+    bonds : dict
+        A dictionary where the keys are tuples of the bonded atom indicies, and the
+        associated values are the bond lengths.
+
     """
 
     if min_bond < 0:
-        raise ValueError("min_bond must be greater than zero")
+        raise ValueError(F"{min_bond} was entered. Minimum bond length\
+             cannot be less than zero.")
 
     bonds = {}
     num_atoms = len(coordinates)
